d2a26e1e8f08ca4e89953b368885c583e47345f5
NathAt996 <n.morgan5196@gmail.com>
2025-11-17 06:45:52 +0000

    Update_Calculator
    The test could not route to the correct source file.

--- src/test/java/runner/CalculatorRunner.java
+++ src/test/java/runner/CalculatorRunner.java
@@ -4,15 +4,12 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-// @RunWith must be on the main class
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/features", // Path to .feature files directory
-        glue = "feature",                         // The package name
+        // CHANGE THIS LINE to use the "classpath:" prefix
+        features = "classpath:features",
+        glue = "feature",
         plugin = {"pretty", "html:target/cucumber-reports.html"}
 )
-
 public class CalculatorRunner {
-
-    // The class body should be empty for a standard Cucumber runner
 }
