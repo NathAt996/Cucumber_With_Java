6c1db8cafe6ccf41b1a5caa0546edc752c1ee2bc
NathAt996 <n.morgan5196@gmail.com>
2025-11-18 06:42:18 +0000

    Update LoginRunner
    Edited out a small typo to ensure code is glued to feature file

--- src/test/java/runner/LoginRunner.java
+++ src/test/java/runner/LoginRunner.java
@@ -4,13 +4,12 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
-// Move the annotations to the top-level public class
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = {"steps"}, // This correctly points to your steps package
+        glue = {"steps"},
         plugin = {"pretty", "html:target/cucumber-reports.html"}
 )
 public class LoginRunner {
-    // This class should be empty. JUnit uses the annotations to run the tests.
+
 }
