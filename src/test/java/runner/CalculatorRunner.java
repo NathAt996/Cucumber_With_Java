0c883d7c9143d56e183d8fb6e053cf8cac8dfb7c
NathAt996 <n.morgan5196@gmail.com>
2025-12-03 08:03:47 +0000

    Runner of Calculator test
    Runner of Calculator test

--- src/test/java/runner/CalculatorRunner.java
+++ src/test/java/runner/CalculatorRunner.java
@@ -6,7 +6,6 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        // CHANGE THIS LINE to use the "classpath:" prefix
         features = "classpath:features",
         glue = "feature",
         plugin = {"pretty", "html:target/cucumber-reports.html"}
